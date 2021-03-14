616b4bb281a61f977caab9f2ee6d596e3d242e3c
vova <vovaforsaz@gmail.com>
2021-03-14 17:31:31 +0300

    python course

--- src/topic1/lesson4/dictionaries.py
+++ src/topic1/lesson4/dictionaries.py
@@ -36,6 +36,11 @@ def dictionary_task1():
     Create new dictionary with contact list - Alex, Igor, Ilon, Nikola.
     Add "Max" to the phonebook with the phone number +38097212199, and remove Alex from the phonebook.
     """
-    my_dictionary = {}
+    my_dictionary = {
+        "Alex": +380972121211,
+        "Igor": +380972121222,
+        "Ilon": +380972121233,
+        "Max": +38097212199
+    }
     # TODO write the code
     return my_dictionary
